f95f7213ecbb1d4234e4ff4c28fc260e908e1539
Koray Ozkal <koryozkal@gmail.com>
2020-07-22 02:19:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ end
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT, 
-        grade TEXT
+        grade INTEGER
       )
     SQL
     DB[:conn].execute(sql)
